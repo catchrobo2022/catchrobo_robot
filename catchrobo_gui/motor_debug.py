27919f6ee53aff3f17b2ca13ef9a0846f1d81ad0
Ok-Rk <riku3141@gmail.com>
2022-07-02 15:58:31 +0900

    add gripper jsk

--- catchrobo_gui/motor_debug.py
+++ catchrobo_gui/motor_debug.py
@@ -21,13 +21,13 @@ class Status(IntEnum):
     COLLISION=5
     FINISH=6
 
-motor_error = [Status.NONE,Status.NONE,Status.NONE]
+motor_error = [Status.NONE,Status.NONE,Status.NONE,Status.NONE]
 start_time = time.time()
-motor_time = [0,0,0]
+motor_time = [0,0,0,0]
 
 def callback(msg):
     motor_error[msg.id] = msg.error_code
-    for i in range(3):
+    for i in range(4):
         if motor_error[i] == Status.FINISH:
             motor_time[i] = time.time() 
         else:
@@ -57,6 +57,14 @@ def check_motor2(stat):
     else:
         stat.summary(diagnostic_msgs.msg.DiagnosticStatus.ERROR, Status(motor_error[2]))
     return stat
+def check_motor3(stat):
+    if motor_error[3] == Status.NONE:
+        stat.summary(diagnostic_msgs.msg.DiagnosticStatus.OK, "OK")
+    elif motor_error[3] == Status.FINISH:
+        stat.summary(diagnostic_msgs.msg.DiagnosticStatus.WARN, Status(motor_error[3]))
+    else:
+        stat.summary(diagnostic_msgs.msg.DiagnosticStatus.ERROR, Status(motor_error[3]))
+    return stat
 
 if __name__=='__main__':
     rospy.init_node('motor_status')
@@ -68,11 +76,12 @@ if __name__=='__main__':
     updater.add("motor0",check_motor0)
     updater.add("motor1",check_motor1)
     updater.add("motor2",check_motor2)
+    updater.add("gripper",check_motor3)
 
     while not rospy.is_shutdown():
         updater.update()
         rospy.sleep(0.1)
-        for i in range(3):
+        for i in range(4):
             if motor_time[i] != 0:
                 if time.time()-motor_time[i] > DERAY:
                     motor_error[i] = Status.NONE
