b200202d87c38863148c06f49740e5d44693cef2
tashiwater <tashiwater@gmail.com>
2022-07-21 00:16:25 +0900

    tf作成

--- catchrobo_manager/src/catchrobo_manager/calibration.py
+++ catchrobo_manager/src/catchrobo_manager/calibration.py
@@ -1,21 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from catchrobo_manager.gui_menu_enum import CalibrationMenu
+
 import rospy
+import tf2_ros
+import tf_conversions
+import geometry_msgs.msg
+from std_msgs.msg import Int8
+
+
+import math
 
 
 class Calibration:
-    def gui_callback(self):
-        ### [TODO] 各辺の測定開始、終了を受け取る
+    def __init__(self):
+        self.FIELD = rospy.get_param("field")
+        name_space = "calibration/"
+        self.BASE_FRAME = rospy.get_param(name_space + "base_frame")
+        self.SHOOTING_BOX_FRAME = rospy.get_param(name_space + "shooting_box_frame")
+        self._br = tf2_ros.StaticTransformBroadcaster()
+        self.update_tf(0, 0, 0)
+        self._gui_msg = CalibrationMenu.NONE
 
-        ### [TODO] 4辺の情報が集まったらtfを更新
+        self._edges = [None] * 4
 
-        pass
+        rospy.Subscriber("calibration_menu", Int8, self.gui_callback, queue_size=3)
+
+    def gui_callback(self, msg):
+        self._gui_msg = msg
+
+        ### [TODO] 4辺の情報が集まっていたらtfを更新
+        for val in self._edges:
+            ### 4辺の情報がどれかまだ取れていなければreturn
+            if val is None:
+                return
+
+        # if self._gui_msg ==
+        self.calc_rectangle()
+        self.update_tf()
 
     def robot_position_callback(self):
-        ### [TODO] 現在値の取得
+        ### [TODO] 測定中なら、現在値を対象の辺に追加
 
         pass
+
+    def calc_rectangle(self):
+        ### [TODO] 取得した点から画像を作成
+
+        ### [TODO] 四角形(矩形)抽出. 中心位置、回転を取得
+        pass
+
+    def update_tf(self, x_m, y_m, theta):
+        t = geometry_msgs.msg.TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = self.BASE_FRAME
+        t.child_frame_id = self.SHOOTING_BOX_FRAME
+
+        t.transform.translation.x = x_m
+        t.transform.translation.y = y_m
+        t.transform.translation.z = 0
+        q = tf_conversions.transformations.quaternion_from_euler(0, 0, theta)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        self._br.sendTransform(t)
 
 
 if __name__ == "__main__":
--- catchrobo_manager/src/catchrobo_manager/gui_menu_enum.py
+++ catchrobo_manager/src/catchrobo_manager/gui_menu_enum.py
@@ -11,3 +11,15 @@ class GuiMenu:
     POINT3 = 5
     INIT = 6
     START = 7
+
+
+class CalibrationMenu:
+    NONE = 0
+    TOP_EDGE_START = 1
+    TOP_EDGE_END = 2
+    DOWN_EDGE_START = 3
+    DOWN_EDGE_END = 4
+    RIGHT_EDGE_START = 5
+    RIGHT_EDGE_END = 6
+    LEFT_EDGE_START = 7
+    LEFT_EDGE_END = 8
