197515745f9ed8b71d844846109ac5d1cddcba06
shimita <tashiwater@gmail.com>
2022-09-17 23:17:40 +0900

    add skip mode

--- catchrobo_manager/src/catchrobo_manager/game_manager.py
+++ catchrobo_manager/src/catchrobo_manager/game_manager.py
@@ -37,6 +37,7 @@ class GameManager:
         self.IS_SIM = rospy.get_param("sim")
         self.SHOOT_HEIGHT_m = rospy.get_param(name_space + "SHOOT_HEIGHT_m")
         self.IS_CONTINUE = rospy.get_param("is_continue")
+        self.SKIP_MODE = rospy.get_param("skip")
         # game_end_topic = rospy.get_param("game_end_topic")
 
         # shooting_box_center_red = rospy.get_param("calibration/shooting_box_center_red")
@@ -164,7 +165,9 @@ class GameManager:
         elif next_action == PickAction.MOVE_Z_SAFE:
             self._robot.go(z=self.INIT_Z_m)
         elif next_action == PickAction.STOP_BEFORE_COMMON:
-            if is_my_area == False and self._old_my_area == True:
+            if self.SKIP_MODE:
+                pass_action = True
+            elif is_my_area == False and self._old_my_area == True:
                 ### 新たに共通エリアに入る場合
                 self._robot.go(x=work_position[0], y=self.BEFORE_COMMON_AREA_Y_m)
                 self._robot.ask_manual()
@@ -258,9 +261,12 @@ class GameManager:
             ### 下ろす
             self._robot.go(z=self.SHOOT_HEIGHT_m)
         elif next_action == ShootAction.PEG_IN_HOLE:
-            ## グリグリ(手動)
-            self._robot.ask_manual()
-            pass_action = True
+            if self.SKIP_MODE and target_id == 2:
+                pass_action = True
+            else:
+                ## グリグリ(手動)
+                self._robot.ask_manual()
+                pass_action = True
             ### ぐりぐり
             # self._robot.peg_in_hole()
             # shoot
